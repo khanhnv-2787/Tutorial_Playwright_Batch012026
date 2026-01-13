b46e2433212686c2957349645f981f244340345e
Khanh Nguyen <nguyen.van.khanh-c@sun-asterisk.com>
2026-01-13 16:08:19 +0700

    feat: BTTH - playwright advance

--- BTTH-playwright/playwright.config.ts
+++ BTTH-playwright/playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -12,68 +12,41 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
-
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+	testDir: "./tests", // thư mục chứa test
+	timeout: 30 * 1000, // timeout mỗi test = 30s
+	retries: 0, // số lần chạy lại test fail (có thể đổi thành 1)
+	reporter: [
+		["list"], // reporter đơn giản trên terminal
+		[
+			"html",
+			{
+				outputFolder: "playwright-report", // nơi xuất báo cáo HTML
+				open: "never", // không tự mở trình duyệt.
+			},
+		],
+		["json", { outputFile: "report.json" }], // lưu kết quả test dạng JSON
+	],
+	use: {
+		// baseURL: "https://saucedemo.com",
+		headless: true, // chạy không hiển thị UI
+		screenshot: "only-on-failure", // chỉ chụp ảnh khi lỗi
+		video: "retain-on-failure", // giữ lại video khi lỗi
+		trace: "on-first-retry", // bật trace nếu test fail lần đầu
+		viewport: { width: 1280, height: 720 },
+		ignoreHTTPSErrors: true,
+	},
+	projects: [
+		{
+			name: "Chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
+		{
+			name: "Firefox",
+			use: { ...devices["Desktop Firefox"] },
+		},
+		{
+			name: "WebKit",
+			use: { ...devices["Desktop Safari"] },
+		},
+	],
 });
